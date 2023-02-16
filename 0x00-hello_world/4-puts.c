c22d6837e9d05e7dbf663465562e9e9cf37eea9d
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-02-16 08:38:25 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/* 
-* we are using a puts and write ""Programming is like building a multilingual puzzle
-* and the program should end with the value 0 for use the return (0)
+/*
+*Enter - main function
+*puts and write ""Programming is like building a multilingual puzzle
+*Return (0)
 */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
