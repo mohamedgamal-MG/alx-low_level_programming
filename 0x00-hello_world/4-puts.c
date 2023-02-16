b4ff42241f0c634bac3dc9258a3e91c4fa6814f1
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-02-16 09:07:15 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*
-*Enter - main function
-*puts and write ""Programming is like building a multilingual puzzle
-*Return (0)
-*/
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
