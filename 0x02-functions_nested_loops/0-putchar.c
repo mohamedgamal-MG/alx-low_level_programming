d265e486ade320caa914b1aacc8671274b5465e6
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-02-21 07:35:15 -0800

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - The function
+ *
+ * Description: print the "_putchar\n"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a[] = "_putchar\n";
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		char b = a[i];
+
+		char(b);
+		i++;
+	}
+	return (0);
+}
