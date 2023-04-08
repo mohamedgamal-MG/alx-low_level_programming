80165585ed544f24222d5ab3e03bf7de137e9ea9
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-04-07 19:58:37 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,21 +4,21 @@
  * binary_to_uint - convert binary number to unsign interger number
  * @b: A pointer to a char of a 0 or 1
  *
- * Return: will out put a the convert number
+ * Return: if b null out 0 Otherwise out put a the convert number
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int weight = 1;
-	int rem;
-	int binary = *b;
+	int len = 0;
 
-	while (b != 0)
+	if (b[len] == '\0')
+		return (0);
+
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		rem = binary % 10;
-		num = num + rem * weight;
-		binary = binary / 10;
-		weight *= 2;
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
 	return (num);
 }
