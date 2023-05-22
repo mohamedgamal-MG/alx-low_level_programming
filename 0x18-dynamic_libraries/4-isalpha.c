759e5a64ce39323bcdd4fc31babbb8ad69685a4a
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-05-22 03:52:11 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _isalpha - function it used
+ * Description: checks for alphabetic character
+ * @c: the integer value it receives
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	int i = 'a';
+	int x = 'A';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		if (c == x)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
